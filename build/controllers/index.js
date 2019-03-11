4bcdb84fccc33f3fd5f95bbc5e59205131179a0d
victorsteven <chikodi543@gmail.com>
2019-03-11 10:33:41 +0100

    Test deployment on heroku

---
+++ build/controllers/index.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var _menus = require('./menus');
+
+var _menus2 = _interopRequireDefault(_menus);
+
+var _meals = require('./meals');
+
+var _meals2 = _interopRequireDefault(_meals);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+module.exports = {
+  menus: _menus2.default,
+  meals: _meals2.default
+};
+//# sourceMappingURL=index.js.map
---
+++ build/utils/dummyMenus.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  menu: {}
+};
+
+// export default {
+//   menu: {
+//     meal1: {
+//       id: 1,
+//       name: 'Fried Rice',
+//       size: 'Medium',
+//       price: '450',
+//     },
+//     meal2: {
+//       id: 2,
+//       name: 'Jollof Rice',
+//       size: 'Large',
+//       price: '550',
+//       menuId: 1,
+//     },
+//     meal3: {
+//       id: 3,
+//       name: 'Jollof Rice',
+//       size: 'Large',
+//       price: '550',
+//       menuId: 1,
+//     },
+//   },
+// };
+//# sourceMappingURL=dummyMenus.js.map
